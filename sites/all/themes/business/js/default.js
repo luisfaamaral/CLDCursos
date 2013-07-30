f981a9368962b01a67eb8926f82c0e3eddcbb0a8
luisfaamaral <luisfernando.amaral@gmail.com>
2013-07-29 21:23:18 -0300

    asd

--- sites/all/themes/business/js/default.js
+++ sites/all/themes/business/js/default.js
@@ -68,13 +68,13 @@ jQuery(document).ready(function() {
 
   //contato
   if (jQuery('form#webform-client-form-13 input#edit-submitted-nome').val() = "") {
-	  jQuery(this).val('Nome')
+	  jQuery('form#webform-client-form-13 input#edit-submitted-nome').val('Nome')
   }
   if (jQuery('form#webform-client-form-13 input#edit-submitted-email').val() == "") {
-	  jQuery(this).val('Email')
+	  jQuery('form#webform-client-form-13 input#edit-submitted-email').val('Email')
   }
   if (jQuery('form#webform-client-form-13 input#edit-submitted-telefone').val() == "") {
-	  jQuery(this).val('Telefone')
+	  jQuery('form#webform-client-form-13 input#edit-submitted-telefone').val('Telefone')
   }
   
   jQuery('form#webform-client-form-13 input#edit-submitted-nome').focus(function() {
